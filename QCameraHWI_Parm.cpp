e2dab8bd65e47047bb4e64cf1343dc1c76fb0770
Guruprasad Gaonkar <ggaonk@codeaurora.org>
2012-07-13 18:14:48 -0700

    Camera: Change to disable fullsizesnapshot flag if h/w does not support.
    Change-Id: I973e03b9798baa715c214bfc8c1161d82871bb19

--- QCameraHWI_Parm.cpp
+++ QCameraHWI_Parm.cpp
@@ -1033,6 +1033,7 @@ void QCameraHardwareInterface::initDefaultParameters()
     if(!rc) {
         ALOGE("%s:LIVESHOT is  not supported", __func__);
         mParameters.set("video-snapshot-supported", "false");
+        mFullLiveshotEnabled = rc;
     } else {
         mParameters.set("video-snapshot-supported", "true");
     }
